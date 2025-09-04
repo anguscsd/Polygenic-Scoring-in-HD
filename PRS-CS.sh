c42c924128e8cd8bdd0104f8e32eaa0e4cbf738a
Angus Dixon <angusdixon27@gmail.com>
2025-09-04 12:57:26 +0100

    Update PRS-CS.sh

--- PRS-CS.sh
+++ PRS-CS.sh
@@ -1,7 +1,9 @@
+
+# all parameters specfic to cardiff university supercomputer HAWK
 #!/bin/bash
-#SBATCH --job-name=run001
+#SBATCH --job-name=run001 
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=c2019487@cardiff.ac.uk
+#SBATCH --mail-user=cxxxxxxx@cardiff.ac.uk
 #SBATCH -t 10:00:00
 #SBATCH --mem=12G
 #SBATCH --ntasks=1
@@ -11,12 +13,12 @@
 module load anaconda
 source activate
 # Define file paths
-sumstat=/home/c.c2019487/met584/data/gwas/Data_for_Angus/sumstats_new_prs
-out=/home/c.c2019487/met584/data/prs_outfiles/
+sumstat=/home/c.cxxxxxxx/met584/data/gwas/Data_for_Angus/sumstats_new_prs
+out=/home/c.cxxxxxxx/met584/data/prs_outfiles/
 n=10552477
-PRSCS=/home/c.c2019487/met584/PRS-CS/PRScs
-bim=/home/c.c2019487/met584/data/gwas/GWA6_test_sample_RSID_new
-ref=/home/c.c2019487/met584/data/eur_ref/ldblk_ukbb_eur
+PRSCS=/home/c.cxxxxxxx/met584/PRS-CS/PRScs
+bim=/home/c.cxxxxxxx/met584/data/gwas/GWA6_test_sample_RSID_new
+ref=/home/c.cxxxxxxx/met584/data/eur_ref/ldblk_ukbb_eur
 # Print assigned chromosome for debugging
 echo "Processing Chromosome: ${SLURM_ARRAY_TASK_ID}"
 # Run PRS-CS for the assigned chromosome
